fbeb7e42c1fd6540dcbafec05b1937a7611f17a6
unknown <sbayu875@gmail.com>
2025-09-26 16:10:41 +0800

    feat: add components card

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,29 @@ import { useState } from "react";
 // import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
     <>
       <Header />
+      <div className="container-md p-4 border">
+        <div className="g-4 row row-cols-1 row-cols-sm-2 row-cols-md-4">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+
       <Footer />
     </>
   );
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Card() {
-  return <div></div>;
+  return (
+    <div className="col">
+      <div className="card h-100">
+        <img src="./iStock.png" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit longer.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
