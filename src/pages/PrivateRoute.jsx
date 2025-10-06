5d446fbb287feb55ee05ebd878ebba7b735a8648
unknown <sbayu875@gmail.com>
2025-10-06 13:48:37 +0800

    fix private route logic

--- src/pages/PrivateRoute.jsx
+++ src/pages/PrivateRoute.jsx
@@ -1,15 +1,20 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "../store/auth-context";
+// import { useContext } from "react";
+// import { AuthContext } from "../store/auth-context";
 
 function PrivateRoute({ allowedRoles }) {
-  const { user } = useContext(AuthContext);
+  // const { user } = useContext(AuthContext);
   const location = useLocation();
+  const token = localStorage.getItem("token");
 
-  // Cek user role
-  if (user && allowedRoles.includes(user.role)) {
-    return <Outlet />;
-  }
+  // Cek login atau tidak
+  if (token) return <Outlet />;
+
+  // // Cek user role
+  // if (user && allowedRoles.includes(user.role)) {
+  //   return <Outlet />;
+  // }
+
   // Jika tidak ada user atau role tidak sesuai, redirect ke halaman login
   return <Navigate to="/login" state={{ from: location }} replace />;
 }
