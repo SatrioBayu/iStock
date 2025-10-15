2896aa50bbca93216afd4d54f037b6df50d9e0c5
unknown <sbayu875@gmail.com>
2025-10-15 11:09:34 +0800

    feat: edit auth-context logout function

--- src/store/auth-context.jsx
+++ src/store/auth-context.jsx
@@ -48,7 +48,7 @@ export const AuthProvider = ({ children }) => {
   const logout = () => {
     localStorage.removeItem("token");
     setUser(null);
-    window.location.href = "/";
+    window.location.href = "/dashboard";
   };
 
   return (
